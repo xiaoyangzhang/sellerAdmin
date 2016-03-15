d57758eed91bcbc074653566497c837b311f2c66
yebin <yebin@yimayholiday.com>
2016-03-15 21:31:34 +0800

    bug fix

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/base/BaseTravelController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/base/BaseTravelController.java
@@ -2,13 +2,9 @@ package com.yimayhd.sellerAdmin.base;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.yimayhd.sellerAdmin.model.line.LineContextConfig;
 import com.yimayhd.sellerAdmin.model.line.LinePropertyConfig;
 import com.yimayhd.sellerAdmin.service.CommLineService;
-import com.yimayhd.sellerAdmin.service.RegionService;
-import com.yimayhd.sellerAdmin.service.UserRPCService;
 
 /**
  * 线路基本Controller
@@ -19,17 +15,13 @@ import com.yimayhd.sellerAdmin.service.UserRPCService;
 public abstract class BaseTravelController extends BaseController {
 	@Resource
 	protected CommLineService commLineService;
-	@Autowired
-	protected RegionService regionService;
-	@Autowired
-	protected UserRPCService userService;
 
 	protected void initBaseInfo() throws BaseException {
 		LineContextConfig lineConfig = commLineService.getLineContextConfig();
 		put("themes", lineConfig.getThemes());
 	}
 
-	protected void initLinePropertyTypes(long categoryId)  {
+	protected void initLinePropertyTypes(long categoryId) {
 		put("categoryId", categoryId);
 		LinePropertyConfig lineConfig = commLineService.getLinePropertyConfig(categoryId);
 		put("options", lineConfig.getOptions());
