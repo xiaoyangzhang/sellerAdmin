758e961eac5bee603971d8e55825681d7e73de42
yebin <yebin@yimayholiday.com>
2016-03-30 13:10:28 +0800

    itemListItem

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/model/item/ItemListItemVO.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/model/item/ItemListItemVO.java
@@ -1,5 +1,10 @@
 package com.yimayhd.sellerAdmin.model.item;
 
+import java.util.Date;
+import java.util.List;
+
+import com.yimayhd.sellerAdmin.model.line.CityVO;
+
 /**
  * 商品列表
  * 
@@ -7,5 +12,94 @@ package com.yimayhd.sellerAdmin.model.item;
  *
  */
 public class ItemListItemVO {
+	private long id;
+	private String picture;
+	private String name;
+	private String code;
+	private List<CityVO> dests;
+	private int itemType;
+	private long price;
+	private long status;
+	private Date publishDate;
+	private List<String> operates;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public List<CityVO> getDests() {
+		return dests;
+	}
+
+	public void setDests(List<CityVO> dests) {
+		this.dests = dests;
+	}
+
+	public int getItemType() {
+		return itemType;
+	}
+
+	public void setItemType(int itemType) {
+		this.itemType = itemType;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	public long getStatus() {
+		return status;
+	}
+
+	public void setStatus(long status) {
+		this.status = status;
+	}
+
+	public Date getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public List<String> getOperates() {
+		return operates;
+	}
+
+	public void setOperates(List<String> operates) {
+		this.operates = operates;
+	}
 }
--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/service/comm/impl/ItemServiceImpl.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/service/comm/impl/ItemServiceImpl.java
@@ -2,12 +2,16 @@ package com.yimayhd.sellerAdmin.service.comm.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import com.alibaba.fastjson.JSON;
+import com.yimayhd.commentcenter.client.domain.ComTagDO;
+import com.yimayhd.commentcenter.client.enums.TagType;
 import com.yimayhd.ic.client.model.domain.item.ItemDO;
 import com.yimayhd.ic.client.model.param.item.ItemQryDTO;
 import com.yimayhd.ic.client.model.result.item.ItemPageResult;
@@ -15,9 +19,13 @@ import com.yimayhd.sellerAdmin.base.BaseException;
 import com.yimayhd.sellerAdmin.base.PageVO;
 import com.yimayhd.sellerAdmin.converter.ItemConverter;
 import com.yimayhd.sellerAdmin.model.item.ItemListItemVO;
+import com.yimayhd.sellerAdmin.model.line.CityVO;
 import com.yimayhd.sellerAdmin.model.query.ItemListQuery;
+import com.yimayhd.sellerAdmin.repo.CityRepo;
+import com.yimayhd.sellerAdmin.repo.CommentRepo;
 import com.yimayhd.sellerAdmin.repo.ItemRepo;
 import com.yimayhd.sellerAdmin.service.comm.ItemService;
+import com.yimayhd.user.client.dto.CityDTO;
 
 /**
  * 商品服务（实现）
@@ -30,6 +38,10 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private ItemRepo itemRepo;
+	@Autowired
+	private CommentRepo commentRepo;
+	@Autowired
+	private CityRepo cityRepo;
 
 	@Override
 	public PageVO<ItemListItemVO> getItemList(long sellerId, ItemListQuery query) {
@@ -40,9 +52,26 @@ public class ItemServiceImpl implements ItemService {
 			throw new BaseException("返回结果错误,新增失败 ");
 		}
 		List<ItemDO> itemDOList = itemPageResult.getItemDOList();
+		List<Long> itemIds = new ArrayList<Long>();
 		List<ItemListItemVO> resultList = new ArrayList<ItemListItemVO>();
 		for (ItemDO itemDO : itemDOList) {
 			resultList.add(ItemConverter.toItemListItemVO(itemDO));
+			itemIds.add(itemDO.getId());
+		}
+		Map<Long, List<ComTagDO>> tagMap = commentRepo.getTagsByOutIds(itemIds, TagType.DESRTPLACE);
+		for (ItemListItemVO itemListItemVO : resultList) {
+			long itemId = itemListItemVO.getId();
+			if (tagMap.containsKey(itemId)) {
+				List<ComTagDO> comTagDOs = tagMap.get(itemId);
+				List<CityVO> dests = new ArrayList<CityVO>();
+				if (CollectionUtils.isNotEmpty(comTagDOs)) {
+					for (ComTagDO comTagDO : comTagDOs) {
+						CityDTO cityDTO = cityRepo.getNameByCode(comTagDO.getName());
+						dests.add(new CityVO(comTagDO.getId(), cityDTO.getName(), cityDTO));
+					}
+				}
+				itemListItemVO.setDests(dests);
+			}
 		}
 		PageVO<ItemListItemVO> pageVO = new PageVO<ItemListItemVO>(query.getPageNo(), query.getPageSize(),
 				itemPageResult.getRecordCount(), resultList);
