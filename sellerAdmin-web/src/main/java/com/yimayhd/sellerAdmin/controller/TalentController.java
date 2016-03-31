74865d1af00dd3a5e9dfc83189480186941b0621
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-03-31 09:59:02 +0800

    达人信息

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/TalentController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/TalentController.java
@@ -1,7 +1,5 @@
 package com.yimayhd.sellerAdmin.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,16 +16,14 @@ import com.alibaba.fastjson.JSON;
 import com.yimayhd.membercenter.client.dto.ExamineInfoDTO;
 import com.yimayhd.membercenter.client.dto.TalentInfoDTO;
 import com.yimayhd.membercenter.client.result.MemResult;
+import com.yimayhd.membercenter.client.service.back.TalentInfoDealService;
 import com.yimayhd.membercenter.client.service.examine.ExamineDealService;
-import com.yimayhd.membercenter.client.service.talent.TalentInfoDealService;
-import com.yimayhd.membercenter.entity.talent.TalentInfo;
 import com.yimayhd.sellerAdmin.base.BaseController;
-import com.yimayhd.sellerAdmin.base.BaseException;
+import com.yimayhd.sellerAdmin.base.ResponseVo;
+import com.yimayhd.sellerAdmin.constant.ResponseStatus;
 import com.yimayhd.sellerAdmin.model.ExamineInfoVO;
-import com.yimayhd.sellerAdmin.model.Region;
 import com.yimayhd.sellerAdmin.model.TalentInfoVO;
 import com.yimayhd.sellerAdmin.service.RegionService;
-import com.yimayhd.user.session.manager.SessionManager;
 /**
  * 达人申请入驻
  * 
@@ -51,7 +47,7 @@ public class TalentController extends BaseController {
 		
 	}
 	/**
-	 * 填写达人申请资料页面1
+	 * 跳转到填写达人申请资料页面1
 	 * 
 	 * @param request
 	 * @param response
@@ -59,17 +55,18 @@ public class TalentController extends BaseController {
 	 * @return
 	 */
 	
-	@RequestMapping(value="userdatafill.htm",method=RequestMethod.GET)
-	public String toUserdatafill1(HttpServletRequest request,HttpServletResponse response,Model model){
+	@RequestMapping(value="userdatafill_a.htm",method=RequestMethod.GET)
+	public String toUserdatafill_a(HttpServletRequest request,HttpServletResponse response,Model model){
 //		try {
 //			List<Region> provinces = regionService.getProvince();
 //			model.addAttribute("provinces", provinces);
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
-		return "system/talent/userdatafill";
+		return "system/talent/userdatafill_a";
 		
 	}
+	
 	
 	@RequestMapping(value="verification.htm",method=RequestMethod.GET)
 	public String verificationPage(HttpServletRequest request,HttpServletResponse response,Model model){
@@ -94,15 +91,27 @@ public class TalentController extends BaseController {
 		vo.setB("bbb");
 		return JSON.toJSONString(vo);
 	}
-	
-	@RequestMapping("saveExamineInfo.do")
-	@ResponseBody
-	public String saveExamineFile(HttpServletRequest request,HttpServletResponse response,Model model,ExamineInfoDTO dto){
+	/**
+	 * 保存资料页面1并跳转到资料页面2
+	 * @param request
+	 * @param response
+	 * @param model
+	 * @param dto
+	 * @return
+	 */
+	@RequestMapping("saveExamineInfo_a.do")
+	//@ResponseBody
+	public String saveExamineFile_a(HttpServletRequest request,HttpServletResponse response,Model model,ExamineInfoDTO dto){
+		return "system/talent/userdatafill_b";
+	}
+	@RequestMapping("saveExamineInfo_b.do")
+	//@ResponseBody
+	public String saveExamineFile_b(HttpServletRequest request,HttpServletResponse response,Model model,ExamineInfoDTO dto){
 		return "system/talent/verification";
 	}
 	@RequestMapping("toAddTalentInfo.htm")
 	public String talentInfo(HttpServletRequest request,HttpServletResponse response,Model model){
-		//model.addAttribute("serviceTypes", talentInfoDealService.queryTalentServiceType());
+		model.addAttribute("serviceTypes", talentInfoDealService.queryTalentServiceType());
 		//MemResult<TalentInfoDTO> talentInfo= talentInfoDealService.queryTalentInfoByUserId(new SessionManager().getUserId(),1200 );
 		/*if (null == talentInfo ) {
 			log.error("talentInfoDealService.queryTalentInfoByUserId result is null and param"+JSON.toJSONString(talentInfo)+"and userId="+new SessionManager().getUserId()+"and domainId="+1200);
@@ -120,9 +129,26 @@ public class TalentController extends BaseController {
 		
 	}
 	@RequestMapping("saveTalentInfo.do")
-	public String saveTalentInfo(HttpServletRequest request,HttpServletResponse response,Model model,TalentInfoDTO dto){
-		MemResult<Boolean> result = talentInfoDealService.updateTalentInfo(dto);
-		return "/success";
+	@ResponseBody
+	public ResponseVo saveTalentInfo(HttpServletRequest request,HttpServletResponse response,Model model,TalentInfoVO vo ){
+		//TalentInfoVO vo = JSON.parseObject(TalentInfoVOStr, TalentInfoVO.class);
+		try {
+			ResponseVo responseVo=new ResponseVo();
+			MemResult<Boolean> result = talentInfoDealService.updateTalentInfo(vo.getTalentInfoDTO(vo));
+			
+			if (result.isSuccess()) {
+				//return "/success";
+				responseVo.setMessage("添加成功！");
+				responseVo.setStatus(ResponseStatus.SUCCESS.VALUE);
+			} else {
+				//responseVo.setMessage(result.getResultMsg());
+				responseVo.setStatus(ResponseStatus.ERROR.VALUE);
+			}
+			return responseVo;
+		} catch (Exception e) {
+			log.error(e.getMessage(),e);
+			return ResponseVo.error(e);
+		}
 		
 	}
 }
