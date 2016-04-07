7338aa386b4046efa952779ba8322210647088c9
yebin <yebin@yimayholiday.com>
2016-04-07 14:52:30 +0800

    bugfix

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/item/ItemController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/item/ItemController.java
@@ -3,12 +3,11 @@ package com.yimayhd.sellerAdmin.controller.item;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +21,7 @@ import com.yimayhd.sellerAdmin.base.result.WebResult;
 import com.yimayhd.sellerAdmin.model.item.ItemListItemVO;
 import com.yimayhd.sellerAdmin.model.query.ItemListQuery;
 import com.yimayhd.sellerAdmin.repo.CategoryRepo;
+import com.yimayhd.sellerAdmin.service.CategoryService;
 import com.yimayhd.sellerAdmin.service.item.ItemService;
 import com.yimayhd.sellerAdmin.vo.menu.CategoryVO;
 import com.yimayhd.stone.enums.DomainType;
@@ -38,7 +38,10 @@ public class ItemController extends BaseController {
 	@Autowired
 	private ItemService itemService;
 	@Autowired
+	private CategoryService categoryServiceRef;
+	@Autowired
 	private CategoryRepo categoryRepo;
+
 	/**
 	 * 商品列表
 	 * 
@@ -47,11 +50,10 @@ public class ItemController extends BaseController {
 	 */
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String list(ItemListQuery query) throws Exception {
-		/*long sellerId = getCurrentUserId();
-		if (sellerId <= 0) {
-			log.warn("未登录");
-			throw new BaseException("请登陆后重试");
-		}*/
+		/*
+		 * long sellerId = getCurrentUserId(); if (sellerId <= 0) {
+		 * log.warn("未登录"); throw new BaseException("请登陆后重试"); }
+		 */
 		long sellerId = 12800;
 		WebResult<PageVO<ItemListItemVO>> result = itemService.getItemList(sellerId, query);
 		if (!result.isSuccess()) {
@@ -62,35 +64,35 @@ public class ItemController extends BaseController {
 		put("query", query);
 		return "/system/comm/itemList";
 	}
-	
+
 	@RequestMapping(value = "/cateList", method = RequestMethod.GET)
-	public String cateList(){
+	public String cateList() {
 		return "/system/comm/category";
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getcate", method = RequestMethod.GET)
-	public List<CategoryVO> getcate(HttpServletRequest request){
-		String cateId = request.getParameter("categoryId");
+	public List<CategoryVO> getcate() {
+		String cateId = get("categoryId");
 		List<CategoryVO> list = null;
-		//查询一级节点
-		if(StringUtils.isBlank(cateId)){
+		// 查询一级节点
+		if (StringUtils.isBlank(cateId)) {
 			CategoryDO categoryDO = categoryRepo.getCategoryByDomainId(DomainType.DOMAIN_JX.getType());
 			list = categoryDoTOVo(categoryDO.getChildren());
-		}else {
-			//查询某节点下的子节点
+		} else {
+			// 查询某节点下的子节点
 			CategoryDO categoryDO = categoryRepo.getCategoryById(Integer.parseInt(cateId));
 			list = categoryDoTOVo(categoryDO.getChildren());
 		}
 		return list;
 	}
-	
-	private List<CategoryVO> categoryDoTOVo(List<CategoryDO> children){
+
+	private List<CategoryVO> categoryDoTOVo(List<CategoryDO> children) {
 		List<CategoryVO> list = new ArrayList<CategoryVO>();
-		if(CollectionUtils.isEmpty(children)){
+		if (CollectionUtils.isEmpty(children)) {
 			return list;
 		}
-		for(CategoryDO categoryDO : children){
+		for (CategoryDO categoryDO : children) {
 			CategoryVO vo = new CategoryVO();
 			vo.setCategoryId(categoryDO.getId());
 			vo.setIsLeaf(categoryDO.getLeaf());
@@ -100,5 +102,25 @@ public class ItemController extends BaseController {
 		}
 		return list;
 	}
-	
+
+	@RequestMapping(value = "/category/{categoryId}/create", method = RequestMethod.GET)
+	public String createItem(@PathVariable(value = "categoryId") long categoryId) throws Exception {
+		CategoryDO categoryDO = categoryServiceRef.getCategoryDOById(categoryId);
+		if (categoryId <= 0 || categoryDO == null) {
+			log.warn("无效categoryId");
+			throw new BaseException("无效categoryId");
+		}
+		ItemType itemType = ItemType.get(categoryDO.getCategoryFeature().getItemType());
+		if (itemType == null) {
+			log.error("category {} ItemType is null", categoryId);
+			throw new BaseException("ItemType is null");
+		}
+		// TODO YEBIN 待开发
+		if (ItemType.FREE_LINE.equals(itemType) || ItemType.TOUR_LINE.equals(itemType)) {
+			return redirect("/line/category/" + categoryId + "/create/");
+		} else {
+			throw new BaseException("unsupport ItemType " + itemType.name());
+		}
+	}
+
 }
