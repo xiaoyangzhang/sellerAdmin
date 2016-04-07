0bd47081a4d8b91f8b4a7cb2df960e9371b75c0d
wuzhengfei <wuzf@yimayholiday.com>
2016-04-07 13:59:41 +0800

    登录页头尾调整

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/util/WebResourceConfigUtil.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/util/WebResourceConfigUtil.java
@@ -21,9 +21,13 @@ public class WebResourceConfigUtil {
 	private final static String RESOURCE_PATH_JIUXIU = "resource.path.jiuxiu";
 	private final static String RESOURCE_PATH_ADMIN = "resource.path.admin";
 	private final static String RESOURCE_PATH = "resource.path";
+	private final static String JIUXIU_OFFICIAL_SITE = "jiuxiu.official.site";
 
 	public static String getRootPath() {
 		return ResourceConfig.getInstance().getValueByKey(ROOT_PATH_KEY);
+	}
+	public static String getJiuXiuOfficialSite() {
+		return ResourceConfig.getInstance().getValueByKey(JIUXIU_OFFICIAL_SITE);
 	}
 
 	public static String getResourcePathJiuXiu() {
