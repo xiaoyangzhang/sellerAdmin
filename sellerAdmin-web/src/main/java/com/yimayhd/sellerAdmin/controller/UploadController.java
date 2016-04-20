0b1d26392ac9bd2673e9976c868cc175ad28da9b
wuzhengfei <wuzf@yimayholiday.com>
2016-04-20 12:17:35 +0800

    bugfix

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/UploadController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/UploadController.java
@@ -1,17 +1,21 @@
 package com.yimayhd.sellerAdmin.controller;
 
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +25,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.alibaba.fastjson.JSON;
 import com.taobao.common.tfs.TfsManager;
+import com.taobao.common.tfs.impl.TfsFile;
 import com.yimayhd.sellerAdmin.base.BaseController;
 import com.yimayhd.sellerAdmin.base.ResponseVo;
 import com.yimayhd.sellerAdmin.base.result.WebResult;
@@ -36,6 +42,7 @@ import com.yimayhd.sellerAdmin.util.excel.TestPerson;
 @Controller
 @RequestMapping("/upload")
 public class UploadController extends BaseController {
+	private static final Logger logger = LoggerFactory.getLogger("UploadController");
 
 	@Autowired
 	private TfsManager tfsManager;
@@ -98,7 +105,83 @@ public class UploadController extends BaseController {
 			return WebResult.failure(WebReturnCode.SYSTEM_ERROR, "上传失败");
 		}
 	}
+	
+    private static final String DOT = ".";
+    private static final int MAX_LENGTH = 4 * 1024 * 1024 ; 
+    
+	
+	private  WebResult<String> upload(HttpServletRequest request, HttpServletResponse response) {
+		WebResult<String> result = new WebResult<String>();
+		try{
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			
+			
+			Iterator<String> iterator = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(iterator.next());
+			String fileName = multipartFile.getOriginalFilename();
+			long size = multipartFile.getSize();
+			if (size > MAX_LENGTH) {
+				result.setWebReturnCode(WebReturnCode.FILE_TO_BIG);
+				return result;
+			}
+			
+			String tfsName = upload(fileName, multipartFile.getBytes(), size);
+			if (tfsName == null) {// 上传失败
+				result.setWebReturnCode(WebReturnCode.UPLOAD_FILE_FAILED);
+				return result;
+			}
+			
+		}catch(Exception e){
+			logger.error("upload failed! ", e);
+		}
+		return result;
+	}
+	
+    private String upload(String fileName, byte[] bytes, long size){
+    	String log = "UUID="+UUID.randomUUID()+"  filename="+fileName+"  size="+size ;
+    	logger.info(log);
+		int index = fileName.lastIndexOf(DOT);
+		String suffix = index != -1 ? fileName.substring(index, fileName.length()) : null;
+		if (StringUtils.isBlank(suffix)) {
+			suffix = ".jpg";
+		}
+		String tfsName = null;
+		try {
+			if (size > MAX_LENGTH) {
+				return null;
+			} else if (size > TfsFile.MAX_SMALL_IO_LENGTH && size < MAX_LENGTH) {
+				String key = String.valueOf(System.currentTimeMillis());
+				tfsName = tfsManager.saveLargeFile(bytes, null, suffix, key);
+			} else {
+				tfsName = tfsManager.saveFile(bytes, null, suffix);
+			}
 
+		} catch (Exception e) {
+			logger.error("upload  filename={}  size=", fileName, size, e);
+		}
+		if (tfsName == null) {// 上传失败
+			return null;
+		}
+		String targetFileName = tfsName + suffix;
+		
+		logger.info(log+"  result="+targetFileName);
+		return targetFileName;
+    }
+    
+	
+
+    @RequestMapping(value = "/file_string", method = RequestMethod.POST)
+    public void uploadFile4String(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	response.setHeader("Content-Type", "text/html; charset=UTF-8");  
+    	response.setHeader("Accept-Charset:", "UTF-8");  
+    	WebResult<String> result = upload(request, response);
+    	OutputStream op = response.getOutputStream();  
+    	op.write(JSON.toJSONString(result).getBytes());
+    	op.close();  
+    	response.flushBuffer(); 
+    }
+	
+	
 	/**
 	 * 上传多个文件
 	 * 
