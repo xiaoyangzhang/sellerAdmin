a2fba25e1633fdf1325abeee571c7f5ff822617b
yebin <yebin@yimayholiday.com>
2016-04-25 17:04:10 +0800

    bug fix

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/BarterItemChecker.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/BarterItemChecker.java
@@ -10,16 +10,18 @@ import java.util.regex.Pattern;
  * Created by czf on 2016/4/7.
  */
 public class BarterItemChecker {
-    //实物商品表单验证
-    public static WebCheckResult BarterItemCheck(ItemVO itemVO){
-        if(itemVO.getTitle() == null)
-            return WebCheckResult.error("商品名称不能为空");
-        if(itemVO.getTitle().length() > 38)
-            return WebCheckResult.error("商品名称不能超过38个字");
-        if(itemVO.getCode() != null && itemVO.getCode().length() > 38)
-            return WebCheckResult.error("商品编码不能超过38个字");
-        //TODO 结合category的验证
-
-        return WebCheckResult.success();
-    }
+	// 实物商品表单验证
+	public static WebCheckResult BarterItemCheck(ItemVO itemVO) {
+		if (itemVO.getTitle() == null)
+			return WebCheckResult.error("商品名称不能为空");
+		if (itemVO.getTitle().length() > 38)
+			return WebCheckResult.error("商品名称不能超过38个字");
+		if (itemVO.getCode() != null && itemVO.getCode().length() > 38)
+			return WebCheckResult.error("商品编码不能超过38个字");
+		WebCheckResult checkPictureText = PictureTextChecker.checkPictureText(itemVO.getPictureTextVO());
+		if (!checkPictureText.isSuccess()) {
+			return checkPictureText;
+		}
+		return WebCheckResult.success();
+	}
 }
