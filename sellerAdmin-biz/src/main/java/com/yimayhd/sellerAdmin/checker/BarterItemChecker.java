f50060ded9b7da1b0bb192d7b95cb753ee4ada0d
yebin <yebin@yimayholiday.com>
2016-04-25 17:11:02 +0800

    bug fix

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/BarterItemChecker.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/BarterItemChecker.java
@@ -3,9 +3,6 @@ package com.yimayhd.sellerAdmin.checker;
 import com.yimayhd.sellerAdmin.checker.result.WebCheckResult;
 import com.yimayhd.sellerAdmin.model.ItemVO;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by czf on 2016/4/7.
  */
@@ -18,10 +15,6 @@ public class BarterItemChecker {
 			return WebCheckResult.error("商品名称不能超过38个字");
 		if (itemVO.getCode() != null && itemVO.getCode().length() > 38)
 			return WebCheckResult.error("商品编码不能超过38个字");
-		WebCheckResult checkPictureText = PictureTextChecker.checkPictureText(itemVO.getPictureTextVO());
-		if (!checkPictureText.isSuccess()) {
-			return checkPictureText;
-		}
 		return WebCheckResult.success();
 	}
 }
