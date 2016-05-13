1529ae2c3714fa728c32d72f97f25e7755108505
王帝 <wangdi@wangdiMacBook-Pro.local>
2016-05-13 19:20:04 +0800

    酒店管理

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/hotelManage/HotelManageController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/hotelManage/HotelManageController.java
@@ -1,52 +1,20 @@
 package com.yimayhd.sellerAdmin.controller.hotelManage;
 
-import com.yimayhd.ic.client.model.domain.item.CategoryDO;
-import com.yimayhd.ic.client.model.enums.ItemType;
 import com.yimayhd.sellerAdmin.base.BaseController;
-import com.yimayhd.sellerAdmin.base.BaseException;
-import com.yimayhd.sellerAdmin.base.PageVO;
-import com.yimayhd.sellerAdmin.base.result.WebOperateResult;
-import com.yimayhd.sellerAdmin.base.result.WebResult;
-import com.yimayhd.sellerAdmin.base.result.WebReturnCode;
-import com.yimayhd.sellerAdmin.enums.BizItemStatus;
-import com.yimayhd.sellerAdmin.enums.BizItemType;
-import com.yimayhd.sellerAdmin.model.enums.ItemOperate;
-import com.yimayhd.sellerAdmin.model.item.ItemListItemVO;
-import com.yimayhd.sellerAdmin.model.query.ItemListQuery;
-import com.yimayhd.sellerAdmin.service.CategoryService;
-import com.yimayhd.sellerAdmin.service.item.ItemService;
-import com.yimayhd.sellerAdmin.vo.menu.CategoryVO;
-import com.yimayhd.stone.enums.DomainType;
-import com.yimayhd.user.client.domain.UserDO;
-import com.yimayhd.user.client.enums.UserOptions;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yimayhd.sellerAdmin.model.HotelManage.HotelMessageVO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
- * 商品管理
+ * 酒店管理
  * 
- * @author yebin
+ * @author wangdi
  *
  */
 @Controller
 @RequestMapping("/hotel")
 public class HotelManageController extends BaseController {
-	@Autowired
-	private ItemService		itemService;
-	@Autowired
-	private CategoryService	categoryService;
 
 	/**
 	 * 商品列表
@@ -55,21 +23,9 @@ public class HotelManageController extends BaseController {
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/list")
-	public String list(ItemListQuery query) throws Exception {
-		long sellerId = getCurrentUserId();
-		if (sellerId <= 0) {
-			log.warn("未登录");
-			throw new BaseException("请登陆后重试");
-		}
-		WebResult<PageVO<ItemListItemVO>> result = itemService.getItemList(sellerId, query);
-		if (!result.isSuccess()) {
-			throw new BaseException(result.getResultMsg());
-		}
-		put("pageVo", result.getValue());
-		put("itemTypeList", BizItemType.values());
-		put("itemStatusList", BizItemStatus.values());
-		put("query", query);
-		return "/system/comm/itemList";
+	public String queryHotelManageList(Model model,HotelMessageVO HotelMessageVO) throws Exception {
+		System.out.println("ddddd");
+		return "/system/comm/hotelManage/choicehotel";
 	}
 
 
