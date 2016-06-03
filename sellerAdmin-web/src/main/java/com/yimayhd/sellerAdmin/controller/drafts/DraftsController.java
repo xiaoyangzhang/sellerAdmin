090fd9f31d3a757023da847c0c2f0a8b287ec98e
xiemn <xiemn@jiuxiulvxing.com>
2016-06-03 09:56:41 +0800

    草稿箱controller

---
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/drafts/DraftsController.java
@@ -0,0 +1,37 @@
+package com.yimayhd.sellerAdmin.controller.drafts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yimayhd.sellerAdmin.base.BaseController;
+import com.yimayhd.sellerAdmin.model.query.DraftListQuery;
+
+/**
+ * 草稿箱
+ *
+ * @author xiemingna
+ *
+ */
+@Controller
+@RequestMapping("/drafts")
+public class DraftsController extends BaseController {
+
+	@RequestMapping(value = "/list")
+	public String list(DraftListQuery query) throws Exception {
+		// long sellerId = getCurrentUserId();
+		// if (sellerId <= 0) {
+		// log.warn("未登录");
+		// throw new BaseException("请登陆后重试");
+		// }
+		// WebResult<PageVO<ItemListItemVO>> result =
+		// itemService.getItemList(sellerId, query);
+		// if (!result.isSuccess()) {
+		// throw new BaseException(result.getResultMsg());
+		// }
+		// put("pageVo", result.getValue());
+		// put("itemTypeList", BizItemType.values());
+		// put("itemStatusList", BizItemStatus.values());
+		// put("query", query);
+		return "/system/comm/itemList";
+	}
+}
