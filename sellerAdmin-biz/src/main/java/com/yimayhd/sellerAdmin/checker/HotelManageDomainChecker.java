3367f152b9c174f0c8451190a603b00953290880
wangdi1 <wangdi1@jiuxiulvxing.com>
2016-06-04 17:10:21 +0800

    酒店管理

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/HotelManageDomainChecker.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/checker/HotelManageDomainChecker.java
@@ -429,6 +429,7 @@ public class HotelManageDomainChecker {
         /***酒店资源信息添加**/
         hotelMessageVO.setName(hotelDO.getName());
         hotelMessageVO.setLocationText(hotelDO.getLocationText());
+        hotelMessageVO.setHotelTypeStr(HotelType.getByType(hotelDO.getType()).getDesc());
         //hotelMessageVO.setSellerId();
         //hotelMessageVO.setCategoryId();
         String phone="";
@@ -442,7 +443,7 @@ public class HotelManageDomainChecker {
 
         ItemFeature itemFeature = itemDO.getItemFeature();
         hotelMessageVO.setHotelId(itemDO.getOutId());//酒店ID
-        hotelMessageVO.setOutType(itemDO.getOutType());//酒店类型
+        hotelMessageVO.setOutType(itemDO.getOutType());//商品类型类型
         hotelMessageVO.setOutTypeStr(ResourceType.getByType(itemDO.getOutType()).getDesc());//酒店类型name
         hotelMessageVO.setTitle(itemDO.getTitle());
         hotelMessageVO.setCode(itemDO.getCode());
