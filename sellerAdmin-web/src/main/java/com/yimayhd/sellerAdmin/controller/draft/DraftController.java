2f866270d3d2990b6e54bc1500e124c9831c5bc1
xiemn <xiemn@jiuxiulvxing.com>
2016-06-07 19:19:29 +0800

    草稿箱

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/draft/DraftController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/draft/DraftController.java
@@ -1,25 +1,23 @@
 package com.yimayhd.sellerAdmin.controller.draft;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
-import com.yimayhd.ic.client.model.domain.item.ItemDO;
-import com.yimayhd.ic.client.model.result.ICResult;
 import com.yimayhd.sellerAdmin.base.BaseController;
 import com.yimayhd.sellerAdmin.base.result.WebOperateResult;
 import com.yimayhd.sellerAdmin.base.result.WebResultSupport;
 import com.yimayhd.sellerAdmin.base.result.WebReturnCode;
 import com.yimayhd.sellerAdmin.checker.LineChecker;
 import com.yimayhd.sellerAdmin.checker.result.WebCheckResult;
-import com.yimayhd.sellerAdmin.constant.Constant;
+import com.yimayhd.sellerAdmin.model.draft.DraftVO;
 import com.yimayhd.sellerAdmin.model.line.LineVO;
 import com.yimayhd.sellerAdmin.model.query.DraftListQuery;
+import com.yimayhd.sellerAdmin.service.draft.DraftService;
 
 /**
  * 草稿箱
@@ -30,6 +28,9 @@ import com.yimayhd.sellerAdmin.model.query.DraftListQuery;
 @Controller
 @RequestMapping("/draft")
 public class DraftController extends BaseController {
+
+	@Autowired
+	private DraftService draftService;
 
 	@RequestMapping(value = "/list")
 	public String list(DraftListQuery query) throws Exception {
@@ -49,15 +50,20 @@ public class DraftController extends BaseController {
 		// put("query", query);
 		return "/system/draft/list";
 	}
-	@RequestMapping(value = "/addLineDraft")
-	public @ResponseBody WebResultSupport addLineDraft(String json,String uuid) {
+
+	@RequestMapping(value = "/saveLineDraft")
+	public @ResponseBody WebResultSupport saveLineDraft(
+			String json,
+			String uuid,
+			@RequestParam(value = "draftId", required = false) Long draftId,
+			@RequestParam(value = "draftName") String draftName) {
 		try {
 			long sellerId = getCurrentUserId();
 			if (sellerId <= 0) {
 				log.warn("未登录");
 				return WebOperateResult.failure(WebReturnCode.USER_NOT_FOUND);
 			}
-			
+
 			if (StringUtils.isBlank(json)) {
 				log.warn("json is null");
 				return WebOperateResult.failure(WebReturnCode.PARAM_ERROR);
@@ -65,14 +71,19 @@ public class DraftController extends BaseController {
 			json = json.replaceAll("\\s*\\\"\\s*", "\\\"");
 			LineVO gt = (LineVO) JSONObject.parseObject(json, LineVO.class);
 			WebCheckResult checkLine = LineChecker.checkLine(gt);
-			if (!checkLine.isSuccess()) {
-				log.warn(checkLine.getResultMsg());
+			DraftVO draftVO = new DraftVO();
+			draftVO.setDraftName(draftName);
+			draftVO.setJsonObject(json);
+			draftVO.setId(draftId);
+//			draftVO.setMainType(DraftEnum.MainType);
+			WebOperateResult result = draftService.saveDraft(json, draftVO);
+			if (!result.isSuccess()) {
 				return checkLine;
 			}
-			long itemId = gt.getBaseInfo().getItemId();
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
-			return WebOperateResult.failure(WebReturnCode.SYSTEM_ERROR, e.getMessage());
+			return WebOperateResult.failure(WebReturnCode.SYSTEM_ERROR,
+					e.getMessage());
 		}
 		return null;
 	}
