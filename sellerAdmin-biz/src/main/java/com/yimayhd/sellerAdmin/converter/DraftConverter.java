53f3b447bc3d6a4231176a3b01312f31334ef3f2
xiemn <xiemn@jiuxiulvxing.com>
2016-06-20 17:58:06 +0800

    草稿箱bugfix

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/converter/DraftConverter.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/converter/DraftConverter.java
@@ -16,15 +16,14 @@ public class DraftConverter {
 	public static LineVO toLineVOWithDraftDetailVO(WebResult<DraftDetailVO> draftDetailVOResult) {
 		DraftDetailVO detailVO = draftDetailVOResult.getValue();
 		String jsonStr = detailVO.getJSONStr();
-		jsonStr = jsonStr.replaceAll("\\\\\"", "\"");
 		LineVO gt = (LineVO) JSONObject.parseObject(jsonStr, LineVO.class);
+		
 		return gt;
 	}
 
 	public static CityActivityItemVO toCityactivityWithDraftDetailVO(WebResult<DraftDetailVO> draftDetailVOResult) {
 		DraftDetailVO detailVO = draftDetailVOResult.getValue();
 		String jsonStr = detailVO.getJSONStr();
-		jsonStr = jsonStr.replaceAll("\\\\\"", "\"");
 		CityActivityItemVO cityActivityItemVO = (CityActivityItemVO) JSONObject.parseObject(jsonStr, CityActivityItemVO.class);
 		return cityActivityItemVO;
 	}
