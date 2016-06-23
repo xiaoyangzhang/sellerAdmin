26cc4e9c11256995f2a3ac085aa1354f5eca8d66
wangdi1 <wangdi1@jiuxiulvxing.com>
2016-06-23 10:02:06 +0800

    合并master 错误修改

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/constant/Constant.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/constant/Constant.java
@@ -23,7 +23,6 @@ public class Constant {
 	public static final int DOMAIN_B2CPC = 1000;
 
 	public static final int DOMAIN_JIUXIU = 1200;
-	public static final int APPID_JIUXIU = 26;
 
 	public static final String ENV_PROD = "prod";
 	public static final int APPID_JIUXIU = 26;//商户后台应用id
