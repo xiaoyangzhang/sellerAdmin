5ead04eebd168d0ff81899e5b7b26773bfec8aa2
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-07-15 16:06:41 +0800

    1

--- sellerAdmin-api/src/main/java/org/yimayhd/sellerAdmin/entity/TalentInfo.java
+++ sellerAdmin-api/src/main/java/org/yimayhd/sellerAdmin/entity/TalentInfo.java
@@ -10,5 +10,10 @@ public class TalentInfo implements Serializable {
 	* @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
 	*/
 	private static final long serialVersionUID = 5270839900414109431L;
-
+	@Description("头像")
+	public String avater;
+	@Description("昵称")
+	public String nickName;
+	@Description("身份认证状态")
+	public int certificateSate;
 }
