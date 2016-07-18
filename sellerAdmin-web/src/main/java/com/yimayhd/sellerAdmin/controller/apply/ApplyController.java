af64f6cd989ea5c48936f1f24b5cb1c6be915c81
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-07-18 17:35:04 +0800

    1

--- sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/apply/ApplyController.java
+++ sellerAdmin-web/src/main/java/com/yimayhd/sellerAdmin/controller/apply/ApplyController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
