c66aaffa57bd58a991ab56208724477b7b98ea3f
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-08-11 14:18:56 +0800

    添加测试

--- sellerAdmin-web/src/test/java/com/yimayhd/sellerAdmin/BaseTest.java
+++ sellerAdmin-web/src/test/java/com/yimayhd/sellerAdmin/BaseTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
-    "classpath:application.xml"
+    "classpath:application-context.xml"
 })
 public class BaseTest {
 
