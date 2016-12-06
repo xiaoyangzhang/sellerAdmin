3b13a116455e90fa0577c0903caa43917cd42e3c
liuxiaopeng <liuxp@jiuxiulvxing.com>
2016-12-06 15:28:49 +0800

    导出列表

--- sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/converter/ExcelExportConverer.java
+++ sellerAdmin-biz/src/main/java/com/yimayhd/sellerAdmin/converter/ExcelExportConverer.java
@@ -86,7 +86,7 @@ public class ExcelExportConverer {
                 bizOrderExportDomain.setConsigneeTel(logisticsOrderDO.getMobilePhone());
                 bizOrderExportDomain.setReceivingProvince(logisticsOrderDO.getProv());
                 bizOrderExportDomain.setReceivingCity(logisticsOrderDO.getCity());
-                bizOrderExportDomain.setReceivingAdress(logisticsOrderDO.getArea());
+                bizOrderExportDomain.setReceivingArea(logisticsOrderDO.getArea());
                 bizOrderExportDomain.setReceivingAdress(logisticsOrderDO.getAddress());
             }
             bizOrderExportDomains.add(bizOrderExportDomain);
